3b401dfb50fd954339d7a3dfa797b3fca947e762
LeoDuffed <carlosmroder@gmail.com>
2025-03-22 23:47:20 -0600

    Apuntes

---
+++ JSconHTML/PrimerPrograma/01-tipos/08-Funciones.js
@@ -0,0 +1,12 @@
+function saludar(){
+    console.log('Hola mundo');
+}
+
+saludar();
+
+function suma(numUno, numDos){
+    return numUno + numDos;
+}
+
+let resultado = suma(10, 10);
+console.log(resultado);
