70d7448946b4d4350dee15b2ec014bcce45772ec
LeoDuffed <carlosmroder@gmail.com>
2025-03-26 14:11:44 -0600

    Update ControlDeFlujo.js

--- JSconHTML/PrimerPrograma/01-tipos/ControlDeFlujo.js
+++ JSconHTML/PrimerPrograma/01-tipos/ControlDeFlujo.js
@@ -1,5 +1,7 @@
 // IF
 
+console.log("Ciclo If");
+
 let edad = 12;
 
 if(edad > 17){
@@ -7,14 +9,44 @@ if(edad > 17){
 } else if (edad > 13) {
     console.log("Usario tiene que estar compañado");
 } else {
-    console.log("No es mayor de edad")
+    console.log("No es mayor de edad");
 }
 
 // While
+
+console.log("Ciclo While");
+
 let i = 0;
 
 while(i < 10){
-    console.log(i)
+
+    if(i%2 == 0){
+        console.log('Numero par', i);
+    }
     i++;
+}
+
+// Do While
+
+console.log("Ciclo Do While");
+
+j = 0;
+do{
+    if(j%2 == 0){
+        console.log('Numero par',j);
+
+    }
+    j++
+
+} while(j < 3);
+
+// FOR
+
+console.log("Ciclo For");
+
+for(let n = 2;n < 10; n++){
+    if(n %2 == 0){
+        console.log('Numero par', n)
+    }
 }
 
