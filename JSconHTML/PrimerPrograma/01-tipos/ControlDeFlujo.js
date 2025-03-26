c05dab897961cabc17720bc78bde728eb841dbab
LeoDuffed <carlosmroder@gmail.com>
2025-03-26 13:53:48 -0600

    Programa de control de flujo

---
+++ JSconHTML/PrimerPrograma/01-tipos/ControlDeFlujo.js
@@ -0,0 +1,12 @@
+// IF
+
+let edad = 25;
+
+if(edad > 17){
+    console.log("Es mayor de edad");
+} else if (edad > 13) {
+    console.log("Usario tiene que estar compañado");
+} else {
+    console.log("No es mayor de edad")
+}
+
