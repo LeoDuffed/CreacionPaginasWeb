72da59b59e932268e105a823289ebc93ca75ec1e
LeoDuffed <carlosmroder@gmail.com>
2025-04-02 14:59:33 -0600

    Ya quedo la primera pagina de la PW

--- PrimeraPaginaWeb/app.js
+++ PrimeraPaginaWeb/app.js
@@ -7,32 +7,50 @@ const navLogo =document.querySelector('#navbar__logo')
 const mobileMenu = () =>{
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
-}
+};
 
 menu.addEventListener('click', mobileMenu);
 
 const higlighMenu = () => {
-    const elem =document.querySelector('.higlight')
-    const homeMenu =document.querySelector('#homa-page')
-    const aboutMenu =document.querySelector('#about-page')
-    const servicesMenu =document.querySelector('#services-page')
+    const elem =document.querySelector('.higlight');
+    const homeMenu =document.querySelector('#homa-page');
+    const aboutMenu =document.querySelector('#about-page');
+    const servicesMenu =document.querySelector('#sevices-page');
 
-    let scrollPos =window.screenY
+    let scrollPos =window.scrollY
 
     if(window.innerWidth > 960 && scrollPos < 600){
-        homeMenu.classList.add('highlight')
-        aboutMenu.classList.remove('highlight')
-        return 
+        homeMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
     } else if (window.innerWidth > 960 && scrollPos < 1400){
-        aboutMenu.classList.add('highlight')
-        homeMenu.classList.remove('highlight')
-        servicesMenu.classList.remove('highlight')
-        return
+        aboutMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        servicesMenu.classList.remove('highlight');
+        return;
     } else if (window.innerWidth > 960 && scrollPos < 2345){
-        servicesMenu.classList.add('highlight')
-        aboutMenu.classList.remove('highlight')
-        return
+        servicesMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
 
     }
 
-}
+    if((elem && window.innerWidth <960 && scrollPos < 600) || elem){
+        elem.classList.remove('highlight');
+    }
+
+};
+
+window.addEventListener('scroll', higlighMenu);
+window.addEventListener('click', higlighMenu);
+
+const hideMenu = () =>{
+    const menuBars = document.querySelector('.is-active');
+    if(window.innerWidth <= 768 && menuBars){
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+};
+
+menuLinks.addEventListener('click', hideMenu);
+navLogo.addEventListener('click', hideMenu);
