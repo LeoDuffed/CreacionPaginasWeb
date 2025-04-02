c0596773d7bbc7c554a1a9127b11f95fb9f52a8e
LeoDuffed <carlosmroder@gmail.com>
2025-04-02 14:42:09 -0600

    Editando cosas en JS

--- PrimeraPaginaWeb/app.js
+++ PrimeraPaginaWeb/app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar-container-menu');
+const navLogo =document.querySelector('#navbar__logo')
 
 //Display mobile function 
 
@@ -9,3 +10,29 @@ const mobileMenu = () =>{
 }
 
 menu.addEventListener('click', mobileMenu);
+
+const higlighMenu = () => {
+    const elem =document.querySelector('.higlight')
+    const homeMenu =document.querySelector('#homa-page')
+    const aboutMenu =document.querySelector('#about-page')
+    const servicesMenu =document.querySelector('#services-page')
+
+    let scrollPos =window.screenY
+
+    if(window.innerWidth > 960 && scrollPos < 600){
+        homeMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return 
+    } else if (window.innerWidth > 960 && scrollPos < 1400){
+        aboutMenu.classList.add('highlight')
+        homeMenu.classList.remove('highlight')
+        servicesMenu.classList.remove('highlight')
+        return
+    } else if (window.innerWidth > 960 && scrollPos < 2345){
+        servicesMenu.classList.add('highlight')
+        aboutMenu.classList.remove('highlight')
+        return
+
+    }
+
+}
